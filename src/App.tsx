a8cc6bf671c72e91e9211c5385ec8ce2ae03f56a
veerprakash28 <veer.prakash_cs.aiml19@gla.ac.in>
2025-06-08 19:12:38 +0530

    removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import Work from "./components/Work";
 function App() {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
-      {/* Grid Background */}
       <div className="fixed inset-0 bg-grid-pattern opacity-5 dark:opacity-10 pointer-events-none"></div>
 
       <Navbar />
@@ -22,7 +21,6 @@ function App() {
         <Contact />
       </main>
 
-      {/* Footer */}
       <footer className="bg-gray-900 dark:bg-black text-white py-8 relative">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex flex-col items-center justify-center">
